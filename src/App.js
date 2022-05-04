b494ff8190b310644bf691bd57ecde64cb57e5a9
Hossain-Sajjad <sunshinems58.com>
2022-05-05 04:55:37 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Hossain Electronics</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#features">Manage Items</Nav.Link>
+                        <Nav.Link href="#features">Add Items</Nav.Link>
+                        <Nav.Link href="#features">My Items</Nav.Link>
+                        <Nav.Link href="#features">Log In</Nav.Link>
+                        <Nav.Link href="#pricing">Register</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
